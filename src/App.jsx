122c58b1a70f000129618773774becb55c034b85
Christian <ChristianArroyo308@yahoo.com>
2024-01-08 10:26:12 -0800

    starting header

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='txt-lg'>Hello World</h1>
+        <div className='flex justify-between'>
+          <div className='flex'>
+            <p>ICON</p>
+            <h1 className='text-blue-950'>Learning Time Adventures Daycare</h1>
+          </div>
+          <div className='flex'>
+            <a href="">Contact</a>
+            <a href="">Contact</a>
+            <a href="">Contact</a>
+          </div>
+        </div>
       </div>
     </>
   )
