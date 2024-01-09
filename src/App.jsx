7010f6cf3bddc6a1586ea810883b1abbea99f587
Christian <ChristianArroyo308@yahoo.com>
2024-01-09 10:18:47 -0800

    header basics done. images folder added to public

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,24 @@ function App() {
 
   return (
     <>
-      <div>
-        <div className='flex justify-between'>
-          <div className='flex'>
-            <p>ICON</p>
-            <h1 className='text-blue-950'>Learning Time Adventures Daycare</h1>
+      <div className='mt-8 px-[6%]'>
+        <header className='flex justify-between items-center'>
+          <div className='flex items-center'>
+            <img className='w-36 h-auto' src="images/adventurelearningtime.png" alt="Company Tree Logo" />
+            <h1 className='text-3xl font-bold text-blue-950'>Learning Time Adventures Daycare</h1>
           </div>
           <div className='flex'>
-            <a href="">Contact</a>
-            <a href="">Contact</a>
-            <a href="">Contact</a>
+            <a className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Home</a>
+            <a className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">About Us</a>
+            <a className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Contact</a>
           </div>
-        </div>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
       </div>
     </>
   )
