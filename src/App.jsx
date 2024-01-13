5def63f8e5b5a0e521d52b0a2a1f84bdd7ffecd6
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-01-12 18:43:52 -0800

    adjusting colors and opacity of div to darken image

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <header className='flex justify-between items-center'>
           <div className='flex items-center'>
             <img className='w-36 h-auto' src="images/adventurelearningtime.png" alt="Company Tree Logo" />
-            <h1 className='text-3xl font-bold text-blue-950'>Learning Time Adventures Daycare</h1>
+            <h1 className='text-3xl font-bold text-blue-700'>Learning Time Adventures Daycare</h1>
           </div>
           <div className='flex'>
             <a className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Home</a>
@@ -21,7 +21,7 @@ function App() {
         <main className='flex flex-col gap-8 justify-center'>
           <section className='relative w-full h-auto'>
             <img className='w-full h-auto' src="images/wix1.webp" alt="Picture of a classroom" />
-            <div className='absolute inset-0 bg-black opacity-20'></div>
+            <div className='absolute inset-0 bg-black opacity-15'></div>
           </section>
           <section className='flex gap-8'>
             <div className='w-1/2 h-64 bg-yellow-400'></div>
