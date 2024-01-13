d1670808735503ef847031264ee49e052662b86e
Christian <ChristianArroyo308@yahoo.com>
2024-01-13 00:28:13 -0800

    progress on next section About Us

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -25,13 +25,24 @@ function App() {
             <h2 className='absolute w-[50rem] font-bold text-center text-white text-7xl leading-tight top-[36%] left-1/2 transform -translate-x-1/2 -translate-y-1/2'>Welcome to Learning Time Adventures Daycare</h2>
           </section>
           <section className='flex gap-6'>
-            <div className='relative w-1/2 bg-gradient-to-b from-orange-200 via-yellow-200 to-yellow-300'>
-              <div>
-                <h2>Our Story</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit sit fugiat, optio aliquid incidunt ea in eum sapiente similique culpa provident molestias obcaecati maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, placeat inventore distinctio molestias, voluptatum repellat fugiat dolores maxime libero ipsam odit in reprehenderit at veritatis?</p>
+            <div className='relative w-1/2 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
+              <div className='absolute flex flex-col gap-4 top-1/4 text-blue-900 text-center'>
+                <h2 className='text-5xl font-bold'>Our Story</h2>
+                <p className='mx-auto w-4/5 text-xl font-light leading-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit sit fugiat, optio aliquid incidunt ea in eum sapiente similique culpa provident molestias obcaecati maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, placeat inventore distinctio molestias, voluptatum repellat fugiat dolores maxime libero ipsam odit in reprehenderit at veritatis?</p>
               </div>
             </div>
             <div className='w-1/2 bg-yellow-400'>
+              <img className='w-full h-auto' src="images/wix2.webp" alt="" />
+            </div>
+          </section>
+          <section className='flex gap-6'>
+            <div className='relative w-1/2 bg-gradient-to-b from-blue-50 via-blue-50 to-blue-200'>
+              <div className='absolute flex flex-col gap-4 top-1/4 text-blue-900 text-center'>
+                <h2 className='text-5xl font-bold'>About Us</h2>
+                <p className='mx-auto w-4/5 text-xl font-light leading-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit sit fugiat, optio aliquid incidunt ea in eum sapiente similique culpa provident molestias obcaecati maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, placeat inventore distinctio molestias, voluptatum repellat fugiat dolores maxime libero ipsam odit in reprehenderit at veritatis?</p>
+              </div>
+            </div>
+            <div className='w-1/2 bg-cyan-400'>
               <img className='w-full h-auto' src="images/wix2.webp" alt="" />
             </div>
           </section>
