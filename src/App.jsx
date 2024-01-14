ddaaced6dce098c13d1347c64db321e820417cdc
Christian <ChristianArroyo308@yahoo.com>
2024-01-14 03:04:24 -0800

    footer first draft

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,10 @@ function App() {
           </div>
         </div>
       </section>
-      <footer></footer>
+      <footer className='flex flex-col mt-10 mb-16 gap-8 text-blue-900 text-center'>
+        <h3>LearningTimeAdventuresDaycare@gmail.com</h3>
+        <small>©2024 by Learning Time Adventures Daycare</small>
+      </footer>
     </>
   )
 }
