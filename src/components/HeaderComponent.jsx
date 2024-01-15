575a1ae04d8b04b8bfe67425964cf9238964ac2e
Christian <ChristianArroyo308@yahoo.com>
2024-01-15 02:49:32 -0800

    header more responsive. Logo shrinks slightly and anchors flex wrap

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -5,10 +5,10 @@ export default function HeaderComponent() {
     <>
         <header className='flex justify-between items-center'>
           <div className='flex items-center'>
-            <img className='w-36 h-auto' src="images/adventurelearningtime.png" alt="Company Tree Logo" />
+            <img className='w-36 min-w-32 h-auto' src="images/adventurelearningtime.png" alt="Company Tree Logo" />
             <h1 className='text-2xl font-bold hidden text-blue-700 md:block xl:text-3xl'>Learning Time Adventures Daycare</h1>
           </div>
-          <div className='flex'>
+          <div className='flex flex-wrap'>
             <a className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Home</a>
             <a className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">About Us</a>
             <a className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Contact</a>
