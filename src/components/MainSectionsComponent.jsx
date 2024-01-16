ff9c70d3d854de054e8aefdde9f315c887429929
Christian <ChristianArroyo308@yahoo.com>
2024-01-16 13:28:19 -0800

    sections look much better on mobile now. Still need to fix margins/padding

--- src/components/MainSectionsComponent.jsx
+++ src/components/MainSectionsComponent.jsx
@@ -9,25 +9,29 @@ export default function MainSectionsComponent() {
             <div className='absolute inset-0 bg-black opacity-20'></div>
             <h2 className='absolute w-4/5 font-bold text-center text-white text-2xl leading-tight top-[36%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:text-5xl lg:text-7xl'>Welcome to Learning Time Adventures Daycare</h2>
           </section>
-          <section className='flex gap-6'>
-            <div className='relative w-1/2 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
-              <div className='absolute flex flex-col gap-4 top-1/4 text-blue-900 text-center'>
-                <h2 className='text-5xl font-bold'>Our Story</h2>
-                <p className='mx-auto w-4/5 text-xl font-light leading-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit sit fugiat, optio aliquid incidunt ea in eum sapiente similique culpa provident molestias obcaecati maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, placeat inventore distinctio molestias, voluptatum repellat fugiat dolores maxime libero ipsam odit in reprehenderit at veritatis?</p>
+          <section className='flex flex-col gap-6 md:flex-row'>
+            <div className='w-full bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300 md:w-1/2'>
+              <div className='h-full items-center flex text-blue-900'>
+                <div className='text-center'>
+                    <h2 className='text-5xl font-bold'>Our Story</h2>
+                    <p className='mx-auto pt-4 w-4/5 text-xl font-light leading-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit sit fugiat, optio aliquid incidunt ea in eum sapiente similique culpa provident molestias obcaecati maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, placeat inventore distinctio molestias, voluptatum repellat fugiat dolores maxime libero ipsam odit in reprehenderit at veritatis?</p>
+                </div>
               </div>
             </div>
-            <div className='w-1/2'>
+            <div className='w-full md:w-1/2'>
               <img className='w-full h-auto' src="images/wix2.webp" alt="" />
             </div>
           </section>
-          <section className='flex gap-6'>
-            <div className='relative w-1/2 bg-gradient-to-b from-blue-50 via-blue-50 to-blue-200'>
-              <div className='absolute flex flex-col gap-4 top-1/4 text-blue-900 text-center'>
-                <h2 className='text-5xl font-bold'>About Us</h2>
-                <p className='mx-auto w-4/5 text-xl font-light leading-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit sit fugiat, optio aliquid incidunt ea in eum sapiente similique culpa provident molestias obcaecati maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, placeat inventore distinctio molestias, voluptatum repellat fugiat dolores maxime libero ipsam odit in reprehenderit at veritatis?</p>
+          <section className='flex flex-col gap-6 md:flex-row'>
+            <div className='w-full bg-gradient-to-b from-blue-50 via-blue-50 to-blue-200 md:w-1/2'>
+              <div className='h-full flex items-center text-blue-900'>
+                <div className='text-center'>
+                    <h2 className='text-5xl font-bold'>About Us</h2>
+                    <p className='mx-auto pt-4 w-4/5 text-xl font-light leading-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit sit fugiat, optio aliquid incidunt ea in eum sapiente similique culpa provident molestias obcaecati maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, placeat inventore distinctio molestias, voluptatum repellat fugiat dolores maxime libero ipsam odit in reprehenderit at veritatis?</p>
+                </div>
               </div>
             </div>
-            <div className='w-1/2'>
+            <div className='w-full md:w-1/2'>
               <img className='w-full h-auto' src="images/wix2.webp" alt="" />
             </div>
           </section>
