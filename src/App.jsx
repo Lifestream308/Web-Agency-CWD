fd49a31c3c2ef8a17e849417dc9f36d78d05029f
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-01-17 12:05:28 -0800

    added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // import { useState } from 'react'
 import './App.css'
-import BGColorSectionComponent from './components/BGColorSectionComponent'
+import { Route, Routes } from 'react-router-dom'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import MainSectionsComponent from './components/MainSectionsComponent'
+import AboutComponent from './components/AboutComponent'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,7 +12,12 @@ function App() {
   return (
     <>
       <HeaderComponent />
-      <MainSectionsComponent />
+
+      <Routes>
+        <Route path='/' element={ <MainSectionsComponent /> } />
+        <Route path='/about' element={ <AboutComponent /> } />
+      </Routes>
+
       <FooterComponent />
     </>
   )
---
+++ src/components/AboutComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutComponent() {
+  return (
+    <div className='text-5xl'>AboutComponent</div>
+  )
+}
