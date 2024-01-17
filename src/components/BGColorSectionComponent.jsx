7ab44c8b8d7200c542b82d3c58904d7e85ef720a
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-01-17 11:12:49 -0800

    BGColorSection now responsive

--- src/components/BGColorSectionComponent.jsx
+++ src/components/BGColorSectionComponent.jsx
@@ -4,12 +4,12 @@ export default function BGColorSectionComponent() {
   return (
     <>
         <section className='mt-6 bg-amber-200 md:mt-16'>
-            <div className='flex gap-6 px-[12%] py-24'>
-                <div className='relative flex justify-center w-1/2'>
+            <div className='flex flex-col-reverse gap-6 items-center px-[12%] py-12 md:flex-row md:py-24'>
+                <div className='relative flex justify-center w-full md:w-1/2'>
                     <div class="absolute w-full h-full bg-blue-500 transform -translate-x-6 translate-y-6"></div>
                     <img className='w-full h-auto z-10' src="images/wix2.webp" alt="" />
                 </div>
-                <div className='flex flex-col px-1 justify-center gap-4 w-1/2 text-blue-800 text-center'>
+                <div className='flex flex-col px-1 justify-center gap-4 w-full text-blue-800 text-center md:w-1/2'>
                     <h2 className='text-5xl font-bold'>Family Business</h2>
                     <h3 className='text-2xl font-semibold'>Outstanding Quality</h3>
                     <p className='text-lg font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo corrupti, vitae est eum praesentium reprehenderit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus officiis ea eum reiciendis. Accusantium maxime doloribus atque ab vitae dignissimos?</p>
