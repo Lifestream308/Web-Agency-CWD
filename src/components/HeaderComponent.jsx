16de1e9d129a6db7f30ced57300bf0d8fee6f5f1
Christian <ChristianArroyo308@yahoo.com>
2024-01-20 02:47:53 -0800

    changed About Us link to just say About

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -11,7 +11,7 @@ export default function HeaderComponent() {
           </div>
           <div className='flex flex-wrap'>
             <Link to='/' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline'>Home</Link>
-            <Link to='/about' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">About Us</Link>
+            <Link to='/about' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">About</Link>
             <Link to='/' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Contact</Link>
           </div>
         </header>
