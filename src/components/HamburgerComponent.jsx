e01cec4e295c3c041aaf3620321fd2c45cce1e4b
Christian <ChristianArroyo308@yahoo.com>
2024-01-22 11:38:39 -0800

    adding links to mobile menu and added opacity to fade it in and out

--- src/components/HamburgerComponent.jsx
+++ src/components/HamburgerComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './navbar.css'
 
 export default function HamburgerComponent() {
@@ -33,13 +33,16 @@ export default function HamburgerComponent() {
                     <div className={burger_class + ' h-2 rounded-lg bg-blue-800'} ></div>
                 </div>
             </nav>
-            <div className={menu_class + ' absolute h-72 bg-white top-32 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200'}></div>
+            <div className={menu_class + ' absolute flex justify-center h-72 bg-white top-32 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200'}>
+                <nav className='pt-6 flex flex-col text-center'>
+                    <Link to='/' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg'>Home</Link>
+                    <Link to='/about' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg' href="">About</Link>
+                    <Link to='/' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg' href="">Contact</Link>
+                </nav>
+            </div>
         </div>
 
-        {/* <nav className='flex flex-wrap md:hidden'>
-            <Link to='/' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline'>Home</Link>
-            <Link to='/about' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">About</Link>
-        </nav> */}
+
     </>
   )
 }
