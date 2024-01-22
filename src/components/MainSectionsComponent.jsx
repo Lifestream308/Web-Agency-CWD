d44277fff3a7cbda2c108b580dafa9a4697ecf07
Christian <ChristianArroyo308@yahoo.com>
2024-01-22 11:08:30 -0800

    changed font size of h2 Welcoming on mobile screens

--- src/components/MainSectionsComponent.jsx
+++ src/components/MainSectionsComponent.jsx
@@ -9,7 +9,7 @@ export default function MainSectionsComponent() {
             <section className='relative w-full h-auto'>
               <img className='w-full h-auto' src="images/wix1.webp" alt="Picture of a classroom" />
               <div className='absolute inset-0 bg-black opacity-20'></div>
-              <h2 className='absolute w-4/5 font-bold text-center text-white text-2xl leading-tight top-[36%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:text-5xl lg:text-7xl'>Welcome to Learning Time Adventures Daycare</h2>
+              <h2 className='absolute w-4/5 font-bold text-center text-white text-3xl leading-tight top-[36%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:text-5xl lg:text-7xl'>Welcome to Learning Time Adventures Daycare</h2>
             </section>
             <section className='flex flex-col gap-6 md:flex-row'>
               <div className='py-10 w-full bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300 md:w-1/2 md:py-4'>
