387753c15516e7ca3475123530d0956be8a20ad1
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-01-23 17:19:32 -0800

    added contact info and hours of operation section

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function ContactComponent() {
   return (
-    <div>ContactComponent</div>
+    <>
+        <div className='mt-8 flex flex-col gap-8 px-[6%]'>
+            <section className='flex flex-col gap-6 md:flex-row'>
+                <div className='py-10 w-full bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300 md:w-1/2 md:py-4'>
+                    <div className='h-full justify-center items-center flex text-blue-900'>
+                        <div className='flex flex-col gap-6 items-center text-center'>
+                            <h2 className='px-2 text-5xl font-bold'>Opening Hours</h2>
+                            <p className='text-xl font-semibold'>Phone: 626-123-4567</p>
+                            <p className='text-xl font-semibold'>Mon-Fri: 8am-5pm</p>
+                            <p className='text-xl font-semibold'>Sat-Sun: Closed</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-full md:w-1/2'>
+                    <img className='w-full h-full object-cover' src="images/wix2.webp" alt="" />
+                </div>
+            </section>
+        </div>
+    </>
   )
 }
