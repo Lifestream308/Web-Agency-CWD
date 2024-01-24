a9b371ff48d22f22c9667a802f389f48ea5e2199
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-01-24 08:40:16 -0800

    added overflow hidden to prevent mobile menu from causing bottom scroll bar

--- src/components/HamburgerComponent.jsx
+++ src/components/HamburgerComponent.jsx
@@ -33,7 +33,7 @@ export default function HamburgerComponent() {
                     <div className={burger_class + ' h-2 rounded-lg bg-blue-800'} ></div>
                 </div>
             </nav>
-            <div className={menu_class + ' absolute flex justify-center h-72 bg-white top-32 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200'}>
+            <div className={menu_class + ' absolute flex justify-center h-72 bg-white top-32 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200 overflow-hidden'}>
                 <nav className='pt-6 flex flex-col gap-2 text-center'>
                     <Link to='/' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg'>Home</Link>
                     <Link to='/about' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg' href="">About</Link>
