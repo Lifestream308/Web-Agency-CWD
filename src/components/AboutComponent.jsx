fb8faec979b471d0bdf1c26e2ef2d4374c0c2992
Christian <ChristianArroyo308@yahoo.com>
2024-02-03 10:54:16 -0800

    fixing styling on educators images to be equal sizes and look good

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -22,21 +22,21 @@ export default function AboutComponent() {
           <h3 className='pt-6 text-xl font-semibold'>Caring & Dedicated</h3>
           <div className='pt-16 flex justify-center gap-6 flex-wrap'>
             <div className='max-w-sm'>
-              <img src="/images/teacher1.jpg" alt="Employee of Learning Time Adventures Daycare" />
+              <img className='w-96 h-96 object-cover object-top' src="/images/teacher1.jpg" alt="Employee of Learning Time Adventures Daycare" />
               <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
                 <h3 className='text-2xl font-bold'>Kimberly</h3>
                 <p className='pt-3 text-xl font-semibold'>Director</p>
               </div>
             </div>
             <div className='max-w-sm'>
-              <img src="/images/teacher2.jpg" alt="Employee of Learning Time Adventures Daycare" />
+              <img className='w-96 h-96 object-cover object-top' src="/images/teacher2.jpg" alt="Employee of Learning Time Adventures Daycare" />
               <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
                 <h3 className='text-2xl font-bold'>Kris Ward</h3>
                 <p className='pt-3 text-xl font-semibold'>Educator</p>
               </div>
             </div>
             <div className='max-w-sm'>
-              <img src="/images/teacher3.jpg" alt="Employee of Learning Time Adventures Daycare" />
+              <img className='w-96 h-96 object-cover object-top' src="/images/teacher3.jpg" alt="Employee of Learning Time Adventures Daycare" />
               <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
                 <h3 className='text-2xl font-bold'>Kris Ward</h3>
                 <p className='pt-3 text-xl font-semibold'>Educator</p>
