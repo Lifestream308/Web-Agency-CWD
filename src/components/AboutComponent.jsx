2254f0f46c953b91a41482508cc5a53859b4b4e5
Christian <ChristianArroyo308@yahoo.com>
2024-02-02 17:13:04 -0800

    updated photos to be of the teachers and changed their titles

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -22,24 +22,24 @@ export default function AboutComponent() {
           <h3 className='pt-6 text-xl font-semibold'>Caring & Dedicated</h3>
           <div className='pt-16 flex justify-center gap-6 flex-wrap'>
             <div className='max-w-sm'>
-              <img src="/images/wix3.webp" alt="Employee of Learning Time Adventures Daycare" />
+              <img src="/images/teacher1.jpg" alt="Employee of Learning Time Adventures Daycare" />
               <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
-                <h3 className='text-2xl font-bold'>Kris Ward</h3>
-                <p className='pt-3 text-xl font-semibold'>Art Teacher</p>
+                <h3 className='text-2xl font-bold'>Kimberly</h3>
+                <p className='pt-3 text-xl font-semibold'>Director</p>
               </div>
             </div>
             <div className='max-w-sm'>
-              <img src="/images/wix3.webp" alt="Employee of Learning Time Adventures Daycare" />
+              <img src="/images/teacher2.jpg" alt="Employee of Learning Time Adventures Daycare" />
               <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
                 <h3 className='text-2xl font-bold'>Kris Ward</h3>
-                <p className='pt-3 text-xl font-semibold'>Art Teacher</p>
+                <p className='pt-3 text-xl font-semibold'>Educator</p>
               </div>
             </div>
             <div className='max-w-sm'>
-              <img src="/images/wix3.webp" alt="Employee of Learning Time Adventures Daycare" />
+              <img src="/images/teacher3.jpg" alt="Employee of Learning Time Adventures Daycare" />
               <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
                 <h3 className='text-2xl font-bold'>Kris Ward</h3>
-                <p className='pt-3 text-xl font-semibold'>Art Teacher</p>
+                <p className='pt-3 text-xl font-semibold'>Educator</p>
               </div>
             </div>
           </div>
