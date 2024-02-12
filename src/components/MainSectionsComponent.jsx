1a322b711f42fe327d32b15297286f33453e2c35
Christian <ChristianArroyo308@yahoo.com>
2024-02-12 03:04:31 -0800

    formatting correctly the About Us section text. Still needs more work

--- src/components/MainSectionsComponent.jsx
+++ src/components/MainSectionsComponent.jsx
@@ -14,19 +14,29 @@ export default function MainSectionsComponent() {
             <section className='flex flex-col gap-6 md:flex-row'>
               <div className='py-10 w-full bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300 md:w-1/2 md:py-4'>
                 <div className='h-full items-center flex text-blue-900'>
-                  <div className='text-center'>
-                      <h2 className='text-5xl font-bold'>About Us</h2>
-                      <p className='mx-auto pt-4 w-4/5 text-xl font-light leading-normal'>We are a licensed CDSS home daycare. Dedicated to providing quality care for all children we serve. We focus on assisting every student in their milestone development. 
-- Phonics/Language  -Sensory 
-- Intro to Spanish       -Intro to Chinese
--Emotional Awareness Development  
--Math Concepts          -Music & Dance
--Science                     -Art 
-
-We also offer 8-hour day care. (full time/ part-time)
--potty-training - indoor/outdoor environment 
--breakfast, hot lunch, am/pm snacks
-</p>
+                  <div className=''>
+                    <h2 className='text-5xl text-center font-bold'>About Us</h2>
+                    <p className='mx-auto pt-4 w-4/5 text-xl font-light leading-normal'>We are a licensed CDSS home daycare. Dedicated to providing quality care for all children we serve. We focus on assisting every student in their milestone development. 
+                    </p>
+                    <div className="flex gap-4">
+                      <ul>
+                        <li>Phonics/Lanuage</li>
+                        <li>Sensory</li>
+                        <li>Intro to Spanish</li>
+                        <li>Intro to Chinese</li>
+                        <li>Emotional Awareness Development</li>
+                      </ul>
+                      <ul>
+                        <li>Math Concepts</li>
+                        <li>Music & Dance</li>
+                        <li>Science</li>
+                        <li>Art</li>
+                      </ul>
+                    </div>
+                    <p className='mx-auto pt-4 w-4/5 text-xl font-light leading-normal'>We also offer 8-hour day care. (full time/ part-time)
+                    -potty-training - indoor/outdoor environment 
+                    -breakfast, hot lunch, am/pm snacks
+                    </p>
                   </div>
                 </div>
               </div>
