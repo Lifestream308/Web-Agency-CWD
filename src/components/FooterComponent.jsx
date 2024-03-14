0b076fb86d4a66ffa3a9219ef66d3ca18dd9d672
Christian <ChristianArroyo308@yahoo.com>
2024-03-14 03:02:05 -0700

    fixed email address

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -4,7 +4,7 @@ export default function FooterComponent() {
   return (
     <>
         <footer className='flex flex-col mt-14 mb-16 gap-8 text-blue-900 text-center'>
-            <h3>LearningTimeAdventuresDaycare@gmail.com</h3>
+            <h3>LearningTimeAdventures@gmail.com</h3>
             <small>©2024 by Learning Time Adventures Daycare</small>
         </footer>
     </>
