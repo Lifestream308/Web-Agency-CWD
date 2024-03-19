3e9fb08ea6bc92ec8c0d28160c01577e9a83952c
Christian <ChristianArroyo308@yahoo.com>
2024-03-19 02:50:53 -0700

    updated days availability and updated text on Enrollment to make more sense

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -11,8 +11,8 @@ export default function ContactComponent() {
                             <h2 className='px-2 text-5xl font-bold'>Opening Hours</h2>
                             <p className='text-sm font-semibold lg:text-lg'>LearningTimeAdventures@gmail.com</p>
                             <p className='text-xl font-semibold'>(626)-391-0408</p>
-                            <p className='text-xl font-semibold'>Mon-Fri: 8:30am-5:00pm</p>
-                            <p className='text-xl font-semibold'>Sat-Sun: Closed</p>
+                            <p className='text-xl font-semibold'>Mon-Thur: 8:30am-5:00pm</p>
+                            <p className='text-xl font-semibold'>Fri-Sun: Closed</p>
                             <p className='text-xl font-semibold'>Conveniently located in West Covina</p>
                         </div>
                     </div>
@@ -28,7 +28,7 @@ export default function ContactComponent() {
                       <h2 className='text-5xl font-bold'>Enrollment</h2>
                       <h3 className='mt-6 italic text-2xl font-semibold'>Now Enrolling. Space is limited.</h3>
                       <p className='mx-auto pt-6 w-4/5 text-xl'>Full-day 8:30am-5:00pm</p>
-                      <p className='mx-auto pt-6 w-4/5 text-xl font-light'>Please let us know which program you are interested in applying for, what allergies your child has, and the current age of your child.</p>
+                      <p className='mx-auto pt-6 w-4/5 text-xl font-light'>Please let us know when you are looking to start, what allergies your child has, and the current age of your child.</p>
                       <p className='mx-auto pt-3 w-4/5 text-xl font-light'>Thank you for your interest in our daycare.</p>
                       <p className='mx-auto pt-3 w-4/5 text-xl font-light'>We'll get back to you as soon as possible!</p>
                   </div>
