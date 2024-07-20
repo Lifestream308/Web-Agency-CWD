9c00acc7be6c67ecff29e589ca1c4d9f610c75e4
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-07-20 13:08:55 -0700

    added bootstrap chat icon to Contact Link in header

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,10 +12,10 @@ export default function HeaderComponent() {
             </Link>
             <h1 className='text-2xl font-bold hidden text-blue-700 md:block xl:text-3xl'>Learning Time Adventures Daycare</h1>
           </div>
-          <nav className='hidden flex-wrap lg:flex'>
+          <nav className='hidden flex-wrap items-center lg:flex'>
             <Link to='/' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline'>Home</Link>
             <Link to='/about' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">About</Link>
-            <Link to='/contact' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Contact</Link>
+            <Link to='/contact' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Contact <i className="bi bi-chat-dots"></i></Link>
           </nav>
           <HamburgerComponent />
         </header>
