99a6e29cf9f57c3d5467a5ac4d5d843c3e4b0852
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-07-28 12:40:47 -0700

    deleted old unused commented out section that used photos of the teachers

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -43,35 +43,6 @@ export default function AboutComponent() {
           <h1 className="text-2xl font-bold mb-8">My Slideshow</h1>
           <SlideshowComponent interval={5000} />
         </div> */}
-
-
-        {/* <section className='text-center'>
-          <h2 className='text-5xl font-bold'>Our Team</h2>
-          <h3 className='pt-6 text-xl font-semibold'>Caring & Dedicated</h3>
-          <div className='pt-16 flex justify-center gap-6 flex-wrap'>
-            <div className='max-w-sm'>
-              <img className='w-96 h-96 object-cover object-top' src="/images/teacher1.jpg" alt="Employee of Learning Time Adventures Daycare" />
-              <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
-                <h3 className='text-2xl font-bold'>Kimberly</h3>
-                <p className='pt-3 text-xl font-semibold'>Director</p>
-              </div>
-            </div>
-            <div className='max-w-sm'>
-              <img className='w-96 h-96 object-cover object-top' src="/images/teacher2.jpg" alt="Employee of Learning Time Adventures Daycare" />
-              <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
-                <h3 className='text-2xl font-bold'>Yvonne</h3>
-                <p className='pt-3 text-xl font-semibold'>Educator</p>
-              </div>
-            </div>
-            <div className='max-w-sm'>
-              <img className='w-96 h-96 object-cover object-top' src="/images/teacher3.jpg" alt="Employee of Learning Time Adventures Daycare" />
-              <div className='py-8 bg-gradient-to-b from-yellow-200 via-yellow-200 to-yellow-300'>
-                <h3 className='text-2xl font-bold'>Stephanie</h3>
-                <p className='pt-3 text-xl font-semibold'>Educator</p>
-              </div>
-            </div>
-          </div>
-        </section> */}
       </div>
     </>
   )
