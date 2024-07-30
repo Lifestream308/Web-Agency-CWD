65753600a26550fe0140426ee6a80e65061d269b
Christian <ChristianArroyo308@yahoo.com>
2024-07-29 18:31:21 -0700

    stripped footer and wrote that it needs to be set

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -4,8 +4,8 @@ export default function FooterComponent() {
   return (
     <>
         <footer className='flex flex-col mt-14 mb-16 gap-8 text-blue-900 text-center'>
-            <h3>LearningTimeAdventures@gmail.com</h3>
-            <small>©2024 by Learning Time Adventures Daycare</small>
+            <h3>SetCompanyEmail@gmail.com</h3>
+            <small>©2024 by Set Company Name</small>
         </footer>
     </>
   )
