b70c90aafc32d0f7b3333bf9497addc91888ad3a
Christian <ChristianArroyo308@yahoo.com>
2024-07-29 18:20:43 -0700

    deleted more unnecessary commented code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
@@ -7,7 +6,6 @@ import AboutComponent from './components/AboutComponent'
 import ContactComponent from './components/ContactComponent'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
