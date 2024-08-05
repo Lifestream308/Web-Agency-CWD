a6d63ac1e58defb51eff984619df698cc5829ac0
Christian <ChristianArroyo308@yahoo.com>
2024-08-04 18:36:24 -0700

    negative margin on relative container seems to push bg color wider than the max width set on the body

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function AboutComponent() {
   return (
     <>
-      <div className='mt-2 flex flex-col gap-6 lg:gap-16 px-[6%] text-blue-900'>
-        <h2>About Page</h2>
+      <div class="relative text-center bg-blue-500 text-white p-4 -mx-96">
+        <h2 className='text-6xl'>About Us</h2>
+        <p>Does this really work? Wow it really does work.</p>
       </div>
     </>
   )
