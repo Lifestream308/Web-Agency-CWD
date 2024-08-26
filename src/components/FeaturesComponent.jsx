cf53630b2d482f0544c859af982c520ce2b4e60a
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-08-26 01:33:36 -0700

    added parallax bg photo to featuresComponent. Custom css needed for bg-image in index.css

--- src/components/FeaturesComponent.jsx
+++ src/components/FeaturesComponent.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function FeaturesComponent() {
   return (
-    <section>
-      <div className='px-[6%] flex flex-col items-center gap-8'>
+    <>
+      <section className='px-[6%] flex flex-col items-center gap-8'>
         <div class="mt-2 py-[2.5rem] min-w-[100vw] text-center flex justify-center bg-fuchsia-300/50">
           <h2 className='font-extrabold text-8xl text-gray-800'>Features</h2>
         </div>
         <div className='max-w-xl text-xl text-center text-gray-600'>
           <p>Our goal is to create quality websites that we can proudly stand behind. Here are the top features that we include with every website.</p>
         </div>
-        <section className='flex justify-center'>
+        <div className='flex justify-center'>
           <div className='p-4 w-1/3 flex flex-col gap-10'>
             <div className='flex flex-col text-gray-700'>
               <div className='px-2 py-1 w-fit border border-green-500 shadow-md shadow-green-500 rounded-full'>
@@ -64,8 +64,14 @@ export default function FeaturesComponent() {
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, voluptatum nihil omnis suscipit culpa veniam!</p>
             </div>
           </div>
-        </section>
-      </div>
-    </section>
+        </div>
+      </section>
+      <section className='relative flex justify-center items-center h-[30rem] bg-fixed bg-cover customBG1'>
+        <div className='absolute w-full h-full bg-black/20'></div>
+        <div className='text-white z-10'>
+          <h3 className='text-3xl font-bold'>Also Included</h3>
+        </div>
+      </section>
+    </>
   )
 }
