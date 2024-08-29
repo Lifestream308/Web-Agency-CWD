fd201d7a399ec7f5540975280fa607d482b076c5
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-08-29 01:20:09 -0700

    added new photos and huge progress on About Us section. Fleshed out paragraphs and styling

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -7,17 +7,20 @@ export default function AboutComponent() {
         <div class="mt-2 py-[2.5rem] min-w-[100vw] text-center flex justify-center bg-blue-200/60">
           <h2 className='font-extrabold text-8xl text-gray-900'>About Us</h2>
         </div>
-        <div className='max-w-2xl text-center'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad numquam repudiandae ipsum necessitatibus aliquid tenetur sapiente recusandae accusantium provident magnam.</p>
+        <div className='max-w-3xl text-center'>
+          {/* <h3 className='mt-16 text-4xl text-gray-700 underline-offset-8'>Welcome To Claremont Web Designs!</h3> */}
         </div>
-        <div className='flex gap-8'>
-          <div className='p-4 w-1/2'>
-            <img className='rounded-lg' src="images/owner1.jpg" alt="" />
+        <div className='mt-14 flex gap-20'>
+          <div className='relative mx-auto my-auto w-[20rem] min-w-[20rem] text-center'>
+            {/* <small className='text-lg text-fuchsia-500'>Claremont Web Designs</small> */}
+            <div className='absolute top-0 left-0 right-0 bottom-0 bg-orange-100/95 -z-10 translate-x-9 translate-y-9 rounded-lg'></div>
+            <img className='w-full h-auto rounded-lg shadow-sm shadow-gray-800' src="images/owner2.jpg" alt="" />
           </div>
-          <div className='p-4 w-1/2 flex flex-col gap-10'>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero est quos dolorem quaerat voluptates in repudiandae cupiditate atque perspiciatis accusamus nihil ex eligendi eum, reiciendis illum at blanditiis aspernatur? Libero!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero est quos dolorem quaerat voluptates in repudiandae cupiditate atque perspiciatis accusamus nihil ex eligendi eum, reiciendis illum at blanditiis aspernatur? Libero!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero est quos dolorem quaerat voluptates in repudiandae cupiditate atque perspiciatis accusamus nihil ex eligendi eum, reiciendis illum at blanditiis aspernatur? Libero!</p>
+          <div className='max-w-xl flex flex-col gap-8 text-xl text-gray-700 leading-7'>
+            <p><span className='font-bold'>Hello, my name is Christian,</span> I am the proud owner and lead developer of Claremont Web Designs. I divide my time between working as a nurse assistant and as a passionate web developer. In both of these roles I practice care, precision, and dedication, every day.</p>
+            <p><span className='font-bold'>At Claremont Web Designs,</span> my mission is to uplift and support the local community by creating high-quality websites tailored specifically to the unique needs of small businesses. I believe that technology should be both affordable and accessible, and I'm dedicated to helping local businesses thrive by building websites that not only look stunning, but also effectively showcase their services and strengths.</p>
+            {/* <p>My journey into web development began years ago as a self-taught developer, driven by curiosity and a desire to help others. Over time, I honed my skills by working with small startup companies, gaining valuable experience that has shaped me into the developer I am today.</p> */}
+            <p><span className='font-bold'>I'm here to partner with you</span> in bringing your vision to life. Together, we can create a website that captures the essence of your business and helps it grow. Let's collaborate to show the world what your business can do!</p>
           </div>
         </div>
       </div>
