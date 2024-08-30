3bbe100ac014b34ca4bef2ccff183a05d5f2d31d
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-08-29 20:32:56 -0700

    working on netlify form submission. Added honeypot and more styling.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -16,20 +16,24 @@ export default function ContactForm() {
       };
 
   return (
-    <div className='mt-12 text-3xl flex justify-center'>
+    <div className='mt-12 text-2xl text-gray-600 flex justify-center'>
         <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact" />
+            <div className='hidden'>
+                <label htmlFor="bot-field">Do not fill out this section</label>
+                <input type="hidden" name="bot-field" />
+            </div>
             <div className='mt-8 flex flex-col'>
                 <label htmlFor="name">Name:</label>
-                <input className='border border-black' type="text" name="name" value={formData.name} onChange={handleChange} required/>
+                <input className='border border-gray-400' type="text" name="name" value={formData.name} onChange={handleChange} required/>
             </div>
             <div className='mt-8 flex flex-col'>
                 <label htmlFor="email">Email:</label> 
-                <input className='border border-black' type="email" name="email" value={formData.email} onChange={handleChange} required/>
+                <input className='border border-gray-400' type="email" name="email" value={formData.email} onChange={handleChange} required/>
             </div>
             <div className='mt-8 flex flex-col'>
                 <label htmlFor="message">Message:</label>
-                <textarea className='border border-black' name="message" value={formData.message} onChange={handleChange} required/>
+                <textarea className='border border-gray-400' name="message" value={formData.message} onChange={handleChange} required/>
             </div>
             <button className='mt-8 px-4 py-2 text-white bg-green-600 rounded-lg' type="submit">Send</button>
         </form>
