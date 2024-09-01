3ba0644ed3da42d0b3166e992ee8930e5637d339
Christian <ChristianArroyo308@yahoo.com>
2024-08-31 22:12:50 -0700

    added mobile menu link to features and h-fit and pb-2

--- src/components/HamburgerComponent.jsx
+++ src/components/HamburgerComponent.jsx
@@ -40,10 +40,11 @@ export default function HamburgerComponent() {
                     <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 rounded-lg bg-gray-800/90'} ></div>
                 </div>
             </nav>
-            <div className={(menuIsOpen? 'w-[12rem] opacity-100' : 'w-0 opacity-0') + ' absolute flex justify-center h-56 bg-white top-28 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200 overflow-hidden'}>
+            <div className={(menuIsOpen? 'w-[12rem] opacity-100' : 'w-0 opacity-0') + ' pb-2 absolute flex justify-center h-fit bg-white top-28 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200 overflow-hidden'}>
                 <nav className='pt-6 flex flex-col gap-2 text-center'>
                     <Link to='/' className='px-4 py-2 text-2xl font-bold text-gray-700 hover:bg-gray-200 rounded-lg'>Home</Link>
                     <Link to='/about' className='px-4 py-2 text-2xl font-bold text-gray-700 hover:bg-gray-200 rounded-lg' href="">About</Link>
+                    <Link to='/features' className='px-4 py-2 text-2xl font-bold text-gray-700 hover:bg-gray-200 rounded-lg' href="">Features</Link>
                     <Link to='/contact' className='px-4 py-2 text-2xl font-bold text-gray-700 hover:bg-gray-200 rounded-lg' href="">Contact</Link>
                 </nav>
             </div>
