9c533a85476588ac3c1d668db1fe8b784e4a9684
Christian <ChristianArroyo308@yahoo.com>
2024-08-31 22:10:09 -0700

    hamburger on mobile view now dark gray and links gray as well

--- src/components/HamburgerComponent.jsx
+++ src/components/HamburgerComponent.jsx
@@ -35,16 +35,16 @@ export default function HamburgerComponent() {
         <div ref={menuRef} className='flex lg:hidden'>
             <nav className='h-20 p-4'>
                 <div className='h-full w-16 flex flex-col justify-between cursor-pointer' onClick={toggleMenu}>
-                    <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 rounded-lg bg-blue-800'} ></div>
-                    <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 rounded-lg bg-blue-800'} ></div>
-                    <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 rounded-lg bg-blue-800'} ></div>
+                    <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 rounded-lg bg-gray-800/90'} ></div>
+                    <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 rounded-lg bg-gray-800/90'} ></div>
+                    <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 rounded-lg bg-gray-800/90'} ></div>
                 </div>
             </nav>
             <div className={(menuIsOpen? 'w-[12rem] opacity-100' : 'w-0 opacity-0') + ' absolute flex justify-center h-56 bg-white top-28 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200 overflow-hidden'}>
                 <nav className='pt-6 flex flex-col gap-2 text-center'>
-                    <Link to='/' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg'>Home</Link>
-                    <Link to='/about' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg' href="">About</Link>
-                    <Link to='/contact' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg' href="">Contact</Link>
+                    <Link to='/' className='px-4 py-2 text-2xl font-bold text-gray-700 hover:bg-gray-200 rounded-lg'>Home</Link>
+                    <Link to='/about' className='px-4 py-2 text-2xl font-bold text-gray-700 hover:bg-gray-200 rounded-lg' href="">About</Link>
+                    <Link to='/contact' className='px-4 py-2 text-2xl font-bold text-gray-700 hover:bg-gray-200 rounded-lg' href="">Contact</Link>
                 </nav>
             </div>
         </div>
