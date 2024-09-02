7a580e5cd41a601601fadef89d8f365cb1f68494
Christian <ChristianArroyo308@yahoo.com>
2024-09-01 18:20:11 -0700

    added flex-col-reverse to hero section. Trying to set up layout for mobile

--- src/components/HomePageComponent.jsx
+++ src/components/HomePageComponent.jsx
@@ -12,13 +12,13 @@ export default function HomePageComponent() {
       <div className='mt-2 flex flex-col gap-8 px-[6%]'>
           <main className='flex flex-col gap-6 justify-center items-center md:gap-16'>
             <section className='min-w-[100vw] flex justify-center bg-orange-100/95'>
-              <div className='px-[8rem] py-[5rem] max-w-[1500px] flex gap-20 justify-center'>
-                <div className='w-1/2 flex flex-col justify-center'>
+              <div className='px-[8rem] py-[5rem] max-w-[1500px] flex flex-col-reverse md:flex-row gap-20 justify-center'>
+                <div className='w-full md:w-1/2 flex flex-col justify-center'>
                   <h3 className='font-bold text-gray-700 text-2xl sm:text-4xl md:text-5xl'>Boosting Local Businesses With Custom Websites</h3>
                   <h3 className='mt-6 hidden font-extralight text-neutral-600 text-base sm:block md:text-lg lg:text-2xl'>Showcase Your Services And Elevate Your Brand With An Affordable Website</h3>
                   <Link to='/contact' className='px-2 py-1 mt-6 w-fit font-normal bg-gradient-to-r from-green-600 to-green-400 text-white hover:text-gray-300 shadow-md shadow-gray-700 rounded-3xl sm:px-4 sm:py-2 sm:text-xl md:text-2xl sm:mt-8' href="">Get In Touch</Link>
                 </div>
-                <div className='w-1/2'>
+                <div className='w-full md:w-1/2'>
                   <img className='w-full h-auto rounded-lg shadow-md shadow-gray-800' width={1280} height={854} src="images/pexels1.jpg" alt="Picture of a happy woman looking at her laptop" />
                 </div>
               </div>
