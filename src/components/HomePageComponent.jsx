408c5177a3a00eefead30bb9238e4307a55c8d33
Christian <ChristianArroyo308@yahoo.com>
2024-09-01 22:17:27 -0700

    be seen/trusted/heard section now has flex-col on small screens

--- src/components/HomePageComponent.jsx
+++ src/components/HomePageComponent.jsx
@@ -28,7 +28,7 @@ export default function HomePageComponent() {
                 <h2 className='px-4 py-6 text-4xl text-center text-gray-700 leading-loose'>Stand <span className='py-5 px-2 text-white bg-gray-700 shadow-md shadow-gray-500 rounded-full'>Out</span><span className=''> From The Competition</span></h2>
                 <p className='mt-10 max-w-4xl text-2xl text-gray-500 text-center'>Your website will improve your visibility, elevate your brand's status, and guide customers through your services.</p>
               </div>
-              <div className='mt-16 flex gap-16 justify-center'>
+              <div className='mt-16 flex flex-col gap-16 justify-center md:flex-row'>
                 <div className='flex flex-col gap-5'>
                   <div className='px-2 py-1 w-fit border border-green-500 shadow-md shadow-green-500 rounded-full'>
                     <i className="material-symbols-outlined text-4xl text-green-500">eyeglasses</i>
