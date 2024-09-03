a7df46595db5b5fce15dfa556bab74f882df70f8
Christian <ChristianArroyo308@yahoo.com>
2024-09-02 21:51:19 -0700

    hero description text size 3xl from 2xl

--- src/components/HomePageComponent.jsx
+++ src/components/HomePageComponent.jsx
@@ -14,7 +14,7 @@ export default function HomePageComponent() {
             <section className='min-w-[100vw] flex justify-center bg-orange-100/95'>
               <div className='px-6 py-4 max-w-[1500px] flex flex-col justify-center gap-12 lg:flex-row lg:gap-20 lg:px-32 lg:py-20'>
                 <div className='w-full lg:w-1/2 flex flex-col text-center justify-center items-center lg:items-start lg:text-left'>
-                  <h3 className='font-bold text-gray-700 text-2xl sm:text-4xl lg:text-5xl'>Boosting Local Businesses With Custom Websites</h3>
+                  <h3 className='font-bold text-gray-700 text-3xl sm:text-4xl lg:text-5xl'>Boosting Local Businesses With Custom Websites</h3>
                   <h3 className='mt-6 hidden font-extralight text-neutral-600 text-base sm:block md:text-lg lg:text-2xl'>Showcase Your Services And Elevate Your Brand With An Affordable Website</h3>
                   <Link to='/contact' className='px-4 py-2 mt-6 w-fit text-xl bg-gradient-to-r from-green-600 to-green-400 text-white hover:text-gray-300 shadow-md shadow-gray-700 rounded-3xl md:text-2xl sm:mt-8' href="">Get In Touch</Link>
                 </div>
