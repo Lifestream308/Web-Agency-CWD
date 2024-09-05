dc9aeac1c50d0bbfc248009dea73da536947e0bd
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-09-05 00:06:28 -0700

    adjusting margins and size of image

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -13,10 +13,10 @@ export default function ContactComponent() {
         <div class="mt-2 py-[2.5rem] min-w-[100vw] text-center flex justify-center bg-green-200/75">
           <h2 className='font-extrabold text-6xl text-gray-800 md:text-8xl'>Contact Us</h2>
         </div>
-        <div className='max-w-xl text-2xl text-center text-gray-600'>
+        <div className='mt-4 max-w-xl text-xl text-center text-gray-600'>
           <p>Send us an email by using our email address down below or by filling out the form and we will get back to you promptly!</p>
         </div>
-        <div className='max-w-4xl'>
+        <div className='mt-4 max-w-3xl'>
           <img className='w-full h-auto shadow-md shadow-gray-700 rounded-lg' src="images/email3.jpg" alt="Picture of a phone focused on emails" />
         </div>
         <h3 className='mt-14 mb-8 text-center text-3xl text-gray-700'>ClaremontWebDesigns@gmail.com</h3>
